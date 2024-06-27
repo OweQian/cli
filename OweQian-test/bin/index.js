58782ae36795899f5af7fc5aeaf9057dc1df283c
qian wang <1510106069@qq.com>
2024-06-27 10:55:53 +0800

    feat: 2-6参数解析

--- OweQian-test/bin/index.js
+++ OweQian-test/bin/index.js
@@ -9,15 +9,26 @@ const argv = require("process").argv;
 const command = argv[2];
 
 const options = argv.slice(3);
-let [option, param] = options;
-option = option.replace("--", "");
 
-if (command) {
-  if (lib[command]) {
-    lib[command]({ option, param });
+if (options.length > 1) {
+  let [option, param] = options;
+  option = option.replace("--", "");
+
+  if (command) {
+    if (lib[command]) {
+      lib[command]({ option, param });
+    } else {
+      console.log("无效的命令");
+    }
   } else {
-    console.log("无效的命令");
+    console.log("请输入命令");
   }
-} else {
-  console.log("请输入命令");
+}
+
+// 实现参数解析 --version 和 init --name
+if (command.startsWith("--") || command.startsWith("-")) {
+  const globalOption = command.replace(/--|-/g, "");
+  if (globalOption === "version" || globalOption === "V") {
+    console.log("1.0.0");
+  }
 }
