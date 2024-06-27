6e03ad02cb6d4dc5a6f80b769f44c3644c13fadf
qian wang <1510106069@qq.com>
2024-06-27 17:26:57 +0800

    feat: commander入门

--- OweQian-test/bin/index.js
+++ OweQian-test/bin/index.js
@@ -1,70 +1,10 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs/yargs");
-const dedent = require("dedent");
+const { Command } = require("commander");
+const pkg = require("../package.json");
+// 单例
+const program = new Command();
 
-const { hideBin } = require("yargs/helpers");
+program.version(pkg.version);
 
-const arg = hideBin(process.argv);
-const cli = yargs(arg);
-cli
-  .usage("Usage: oweqian-test [command] <options>")
-  .demandCommand(
-    1,
-    "A command is required. Pass --help to see all available commands and options"
-  )
-  .strict()
-  .recommendCommands()
-  .fail((err, msg) => {
-    console.log(err);
-  })
-  .alias("h", "help")
-  .alias("v", "version")
-  .wrap(cli.terminalWidth())
-  .epilogue(
-    dedent`Leading and trailing lines will be trimmed, so you can write something like
-  this and have it work as you expect:
-
-    * how convenient it is
-    * that I can use an indented list
-       - and still have it do the right thing
-
-  That's all.`
-  )
-  .options({
-    debug: {
-      type: "boolean",
-      describe: "Bootstrap debug mode",
-      alias: "d",
-    },
-  })
-  .option("registry", {
-    type: "string",
-    describe: "Define global registry",
-    alias: "r",
-  })
-  .group(["debug"], "Dev Options:")
-  .group(["registry"], "Extra Options:")
-  .command(
-    "init [name]",
-    "Do init a project",
-    (yargs) => {
-      yargs.option("name", {
-        type: "string",
-        describe: "Name of a project",
-        alias: "n",
-      });
-    },
-    (argv) => {
-      console.log(argv);
-    }
-  )
-  .command({
-    command: "list",
-    aliases: ["ls", "la", "ll"],
-    describe: "List local packages",
-    builder: (yargs) => {},
-    handler: (argv) => {
-      console.log(argv);
-    },
-  }).argv;
+program.parse();
