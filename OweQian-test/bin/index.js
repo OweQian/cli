a7b4dbcbdb342252e170ce7c29ebb32773812431
qian wang <1510106069@qq.com>
2024-06-29 14:47:28 +0800

    feat: chalk

--- OweQian-test/bin/index.js
+++ OweQian-test/bin/index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-require("./inquirer/ansi");
+// import chalk, { Chalk } from "chalk";
+
+// console.log(chalk.red("hello world"));
+// console.log(chalk.red("hello world") + "!" + chalk.blue("hello world"));
+// console.log(chalk.red.bgGreen.bold("hello world"));
+// console.log(chalk.red("hello", "world"));
+// console.log(chalk.red("hello", chalk.underline("world")));
+// console.log(chalk.rgb(255, 255, 0).underline("hello world"));
+// console.log(chalk.hex("#ff0000").underline("hello world"));
+
+// const error = (...text) => console.log(chalk.bold.hex("#ff0000")(text));
+// const warning = (...text) => console.log(chalk.bold.hex("#ffa500")(text));
+
+// const customChalk = new Chalk({ level: 0 });
