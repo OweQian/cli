2d7f4da7cda73ba2dbb7ec214abe18acad816f1b
qian wang <1510106069@qq.com>
2024-07-02 20:26:20 +0800

    feat: lerna init

---
+++ lerna/packages/cli/bin/cli.mjs
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+"use strict";
+
+// import myCli from "../src/cli.mjs";
+
+// // eslint-disable-next-line no-unused-expressions
+// myCli().parse(process.argv.slice(2));
+import a from "@oweqian/lerna-test-a";
+import b from "@oweqian/lerna-test-b";
+
+console.log(a());
+console.log(b());
